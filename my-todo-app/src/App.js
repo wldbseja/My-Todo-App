03cfb1848f9ce49ca33f2146e552c469c3ab25b7
wldbseja <wldbseja@daum.net>
2023-06-12 17:25:33 +0900

    style(App) : 문구 수정

--- my-todo-app/src/App.js
+++ my-todo-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Todo App에 FE 프레임워크 적용 과제 진행
         </p>
         <a
           className="App-link"
